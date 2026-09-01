631d13d8ffc8f9d00dbfda5e216b9a7347c44873
Elisa Kazan <elisa.kazan@gmail.com>
2026-09-01 14:14:00 -0700

    Initial Commit

---
+++ GlimmerTests/GlimmerTests.swift
@@ -0,0 +1,19 @@
+//
+//  GlimmerTests.swift
+//  GlimmerTests
+//
+//  Created by Elisa Kazan on 2026-09-01.
+//
+
+import Testing
+@testable import Glimmer
+
+struct GlimmerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
